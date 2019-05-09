c9d81e583bf157cf08f557499f72779814505a97
ati <atahir.ince@gmail.com>
2019-05-09 23:38:39 +0300

    [create other language list for ready to write into .txt file as we want]

--- App1.py
+++ App1.py
@@ -1,13 +1,9 @@
 import os
 import codecs
 
-#sample read a file... with true codec...
-''' 
-f = codecs.open('Str-EN_O2.txt', encoding='cp857') # cp857 for turkish
-for line in f:
-    print ((line))
-'''
+SelectedEncoding= 'cp857' # cp857 for turkish, which created first...  Can be change after which is fitted.. .
 
+#sample read a file... with true codec...
 #### NOTE: One project folder have to 2 file and other has to 1 file. And 1 file should be referance for string matching and creating...
 
 ProjFolderName=['G8', 'O2']
@@ -30,20 +26,75 @@ for i in range(len(ProjFolderName)):
 
 #then, we choose 1 file folder should be referance for string ordeer..
 # lets take ref file for order
-directoryF=ProjDirs[(StringFileNumber[0] > StringFileNumber[1])]
+directoryF=ProjDirs[(StringFileNumber[0] > StringFileNumber[1])] #ref file
+langdirectoryF=ProjDirs[(StringFileNumber[0] < StringFileNumber[1])] #which other lang file exist...
 for name in os.listdir(directoryF):
     if ".txt" in name:
         RefFile=directoryF+'\\'+name
-        print(RefFile) # finally, get the file which ref for us...
-
-
-
-
-
-
-
-'''
-files = os.listdir(Proj2Dir)
-for name in files:
+        print("ref file: "+ RefFile) # finally, get the file which ref for us...
+for name in os.listdir(langdirectoryF):
     if ".txt" in name:
-        print(name) '''
+        if name!=RefFile.split("\\")[-1]:#last element
+            LangFile = langdirectoryF + '\\' + name
+            print("lang file: " + LangFile) # which other lang file wil use for creatin.. .
+
+#lets get the string referance from refFile
+
+f = codecs.open(RefFile, encoding=SelectedEncoding) # cp857 for turkish, which created first...
+RefFileStringCode=[]
+RefFileStringInfo=[]
+for line in f:
+    bufStr=str(line).split(';')
+    RefFileStringCode.append(bufStr[0]) #name of string kind of , it is referance string name list and order...
+    bufStr2=bufStr[1].split('"')
+    RefFileStringInfo.append(bufStr2[1])
+f.close()
+
+# we get the ref list from RefFile... Now get the Othe rlang file for read and filled..
+
+f_other = codecs.open(LangFile, encoding=SelectedEncoding)
+LangFileStringCode=[]
+LangFileStringInfo=[]
+for line in f_other:
+    bufStr=str(line).split(';')
+    LangFileStringCode.append(bufStr[0]) #name of string kind of , it is referance string name list and order...
+    bufStr2 = bufStr[1].split('"')
+    LangFileStringInfo.append(bufStr2[1])
+f_other.close()
+
+print("finito file list things........................")
+
+# lets create new file list
+FileStringCode=[]
+FileStringInfo=[]
+
+# we use RefFile for ref : StringCode with Length...
+for ref in range(len(RefFileStringCode)):
+    for lan in range(len(LangFileStringCode)):
+        if LangFileStringCode[lan]==RefFileStringCode[ref]:
+            FileStringCode.append(RefFileStringCode[ref])
+            FileStringInfo.append(LangFileStringInfo[lan])
+            break
+    if (len(FileStringCode)<=ref):
+        FileStringCode.append(RefFileStringCode[ref])
+        FileStringInfo.append(RefFileStringInfo[ref])
+
+print("ref file string code and info: " + str(len(RefFileStringCode)))
+print("new file string code: " + str(len(FileStringCode)) + "||" + "new file string info : " + str(len(FileStringInfo)))
+
+#lets test is it correct?
+
+for xx in range(len(RefFileStringInfo)):
+    if RefFileStringCode[xx]==FileStringCode[xx]:
+        print("TRUE || "  + RefFileStringInfo[xx] + " || " + FileStringInfo[xx] )
+    else:
+        print(" *** FALSE *** || " + RefFileStringInfo[xx] + " || " + FileStringInfo[xx])
+
+
+# ok we create other lang list and lets fill the new file
+
+
+
+
+
+
